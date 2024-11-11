516dee17cdd70e105f51cec6e8f7e63e86840b30
Moonbrite <josemiguelvalecosta@gmail.com>
2024-11-11 16:51:44 +0100

    Ajout de donner de test

---
+++ src/main/java/org/example/e_commerce/initialization/AddingData.java
@@ -0,0 +1,210 @@
+package org.example.e_commerce.initialization;
+
+import lombok.AllArgsConstructor;
+import org.example.e_commerce.model.*;
+import org.example.e_commerce.repository.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.e_commerce.util.HashPasswordUser.generateSalt;
+import static org.example.e_commerce.util.HashPasswordUser.hashPassword;
+
+@Component("addingData")
+@Profile("dev")
+@AllArgsConstructor
+public class AddingData implements CommandLineRunner {
+
+    private CartItemRepository cartItemRepository;
+    private CartRepository cartRepository;
+    private OrderItemRepository orderItemRepository;
+    private OrderRepository orderRepository;
+    private PaymentRepository paymentRepository;
+    private ProductRepository productRepository;
+    private UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+
+        Instant instantDebut = Instant.now();
+
+        System.out.println("============= Ajout des donnee start ===============");
+
+        createUser();
+        createProduct();
+        createCart();
+        createCartItem();
+        createOrder();
+        createOrderItem();
+        createPayment();
+
+        Instant instantFin = Instant.now();
+
+        System.out.println("Ajouts effectués en " + (instantFin.toEpochMilli() - instantDebut.toEpochMilli()) + " ms");
+
+
+        System.out.println("====================================================");
+
+    }
+
+    private void createUser() {
+
+        User user = new User();
+        user.setFirstName("José");
+        user.setLastName("Vale Costa");
+        user.setRole("ROLE_USER");
+        user.setEmail("test@test.fr");
+
+        try {
+            String password = "test";
+            // Générer un salt
+            byte[] salt = generateSalt();
+            // Hacher le mot de passe avec le salt
+            String hashedPassword = hashPassword(password, salt);
+            user.setPassword(hashedPassword);
+            user.setSalt(salt);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        userRepository.save(user);
+
+    }
+
+    private void createProduct() {
+
+        Product product = new Product();
+
+        product.setName("Vue sur mer");
+        product.setStock(25);
+        product.setPrice(29.99);
+        product.setImageUrl("https://www.vuesurmer.fr/images/plage-saleccia.jpg");
+        product.setDescription("Une vue côtière à couper le souffle dévoile une mer d'un bleu turquoise éclatant qui s'étend à perte de vue jusqu'à l'horizon.");
+
+        productRepository.save(product);
+
+    }
+
+    private void createCart() {
+
+        Cart cart = new Cart();
+
+        User user = userRepository.findByEmail("test@test.fr");
+
+        cart.setUser(user);
+        user.setCart(cart);
+
+        userRepository.save(user);
+
+    }
+
+    private void createCartItem() {
+
+        int quantity = 1;
+
+        Cart cart = cartRepository.findById(1L).orElse(null);
+        Product product = productRepository.findById(1L).orElse(null);
+
+        CartItem cartItem = new CartItem();
+        cartItem.setCart(cart);
+        cartItem.setProduct(product);
+        cartItem.setQuantity(quantity);
+
+        assert product != null;
+        cartItem.setPrice(product.getPrice() * quantity);
+
+        cartItemRepository.save(cartItem);
+
+    }
+
+    private void createOrder(){
+
+        User user = userRepository.findByEmail("test@test.fr");
+
+
+        Order order = new Order();
+
+        order.setOrderDate(LocalDateTime.now());
+        order.setUser(user);
+        order.setPaymentStatus("Paye");
+        order.setTotalPrice(236.0);
+
+        orderRepository.save(order);
+
+
+    }
+
+    private void  createOrderItem() {
+
+        int quantity = 1;
+
+        List<OrderItem> itemList = new ArrayList<>();
+        Product product = productRepository.findById(1L).orElse(null);
+        Order order = orderRepository.findById(1L).orElse(null);
+
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setProduct(product);
+        orderItem.setOrder(order);
+
+        assert product != null;
+        orderItem.setPrice(product.getPrice() * quantity);
+        orderItem.setQuantity(quantity);
+
+        itemList.add(orderItem);
+
+        orderItemRepository.save(orderItem);
+
+
+        OrderItem orderItem2 = new OrderItem();
+
+        orderItem2.setProduct(product);
+        orderItem2.setOrder(order);
+
+        assert product != null;
+        orderItem2.setPrice(product.getPrice() * quantity);
+        orderItem2.setQuantity(quantity);
+
+        itemList.add(orderItem2);
+
+        orderItemRepository.save(orderItem2);
+
+
+
+        assert order != null;
+        order.setItems(itemList);
+        orderRepository.save(order);
+
+
+    }
+
+    private void createPayment() {
+
+        Payment payment = new Payment();
+        Order order = orderRepository.findById(1L).orElse(null);
+
+        payment.setPaymentIntentId("idStripeHere");
+        payment.setOrder(order);
+
+        List<OrderItem> orderItemList = orderItemRepository.findByOrderId(1L);
+
+        Double amountTotal = orderItemList.stream()
+                .mapToDouble(OrderItem::getPrice)
+                .sum();
+
+        payment.setAmount(amountTotal);
+        payment.setStatus("paye");
+        payment.setCurrency("euro");
+
+        paymentRepository.save(payment);
+    }
+
+}
--- src/main/java/org/example/e_commerce/repository/OrderItemRepository.java
+++ src/main/java/org/example/e_commerce/repository/OrderItemRepository.java
@@ -4,7 +4,9 @@ import org.example.e_commerce.model.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem,Long> {
-
+    List<OrderItem> findByOrderId(Long orderId);
 }
